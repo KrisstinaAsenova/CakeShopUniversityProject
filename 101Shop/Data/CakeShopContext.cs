4ba708bc49075f01651881f9888aae25d487b608
Kristina Asenova <krisstina.asenova@gmail.com>
2020-03-21 20:34:11 +0200

    CakeShop

---
+++ 101Shop/Data/CakeShopContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using _101Shop.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace _101Shop.Data
+{
+    public class CakeShopContext : IdentityDbContext<User>
+    {
+        public CakeShopContext(DbContextOptions<CakeShopContext> options)
+            : base(options)
+        {
+        }
+    }
+}
