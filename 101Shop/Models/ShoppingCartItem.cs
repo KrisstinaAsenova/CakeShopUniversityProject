41abb9b7d62e4396bd1f1ff60b7dc506d50b4ecd
Toni Stoyanov <tohnkz@abv.bg>
2020-05-29 20:00:30 +0300

    random fix

--- 101Shop/Models/ShoppingCartItem.cs
+++ 101Shop/Models/ShoppingCartItem.cs
@@ -7,6 +7,7 @@ namespace _101Shop.Models
 {
     public class ShoppingCartItem
     {
+        public int ShoppingCartItemId { get; set; }
         public Cake Cake { get; set; }
         public int Amount { get; set; }
         public string ShoppingCartId { get; set; }
